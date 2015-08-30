74f2deca8fa1957bd97e6c553bc1ec7a09cae4b9
Roger Gammans <rgammans@gammascience.co.uk>
2015-08-30 16:29:05 +0100

    Proof of concept for Save and load hooks.

---
+++ src/simpleFile.js
@@ -0,0 +1,35 @@
+//Learn to use browerify or whaterve to do a proper jQuery import
+// $ - is assumed to be jQuery
+
+class SimpleFileReader {
+
+    constructor(container,id) { 
+        var that = this;
+        this.container = container
+        $(container).html('<input type="file" id="' + id +'" name="file" > <div id="xx" style="background:grey;" class="content"> ');
+        var x=$("#"+id,container);
+        $("#"+id,container).on('change',function() { alert('xa'); that.onchanged.apply(that,arguments) } );
+    }
+    onchanged(e) {
+        alert('c');
+        var that = this;
+        this.f = e.target.files[0];
+        this.reader = new FileReader();
+        this.reader.onabort = function() { that.onabort.apply(that,arguments) };
+        this.reader.onerror = function() { that.onerror.apply(that,arguments) };
+        this.reader.onload =  function() { that.onload.apply(that,arguments) };
+        this.reader.readAsText(this.f);
+        this.container.inner_html="loading...";
+    }
+
+    onabort (evt) {
+        $('#xx',this.container).html('<div class="error abort"> The file load failed (abort)</div>')
+    }
+    onabort (evt) {
+        $('#xx',this.container).html('<div class="error "> The file load failed (error)</div>')
+    }
+    onload (evt) {
+        var txt =  this.reader.result;
+        $('#xx',this.container).html('<div class="data">'+ this.reader.result +'</div>')
+    }
+}
